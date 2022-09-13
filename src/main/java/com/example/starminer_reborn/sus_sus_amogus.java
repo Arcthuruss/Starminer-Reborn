02d7416cf0a647a279365a828526a8c3db28abbe
Luc Donnard <luc.donnard1108@gmail.com>
2022-09-13 08:34:55 +0200

    configured mod info

---
+++ src/main/java/com/example/starminer_reborn/sus_sus_amogus.java
@@ -0,0 +1,9 @@
+package com.example.starminer_reborn;
+
+import net.minecraft.block.Block;
+
+public class sus_sus_amogus extends Block {
+    public sus_sus_amogus(Properties properties) {
+        super(properties);
+    }
+}
