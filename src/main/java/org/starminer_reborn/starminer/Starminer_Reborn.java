b2814d953e75a6d8caaced50fa9b89d3e732ee65
KoiFeur <KoiFeur@users.noreply.github.com>
2023-04-14 12:12:42 +0200

    feur

--- src/main/java/org/starminer_reborn/starminer/Starminer_Reborn.java
+++ src/main/java/org/starminer_reborn/starminer/Starminer_Reborn.java
@@ -36,6 +36,7 @@ public class Starminer_Reborn {
 
         //comme dit le commentaire d'EnderIO, ici on enregistre tout comme ça tout est enregirtré (c'est pas la bonne def de register mais ça me fait rire
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
 
 
